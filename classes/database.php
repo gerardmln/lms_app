bc26f0394e3e31cb6ea82dd983760cf9ebe74bd8
gerardmln <malaluangl@students.nu-lipa.edu.ph>
2025-05-08 16:04:32 +0800

    first commit

---
+++ classes/database.php
@@ -0,0 +1,63 @@
+<?php
+
+class database{
+
+    function opencon(): PDO{
+
+        return new PDO(
+       dsn: 'mysql:host=localhost;
+        dbname=lms_app',
+        username:'root',
+        password: '');
+
+
+    }
+
+    function signupUser($firstname, $lastname, $birthday, $sex, $email, $phone, $username, $password, $profile_picture_path){
+
+        $con = $this->opencon();
+        
+        try{
+
+            $con->beginTransaction();
+            $stmt = $con->prepare("INSERT INTO users(user_FN, user_LN, user_birthday, user_sex, user_email, user_phone, user_username, user_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt->execute([$firstname, $lastname, $birthday, $sex, $email, $phone, $username, $password]);
+            $userID = $con->lastInsertId();
+
+            $stmt = $con->prepare("INSERT INTO users_pictures(user_id, user_pic_url) VALUES (?, ?)");
+            $stmt->execute([$userID, $profile_picture_path]);
+
+            $con->commit();
+            return $userID;
+        } catch (PDOException $e){
+            $con->rollback();
+            return false;
+
+        }
+        
+
+    }
+
+    function insertAddress($userID, $street, $barangay, $city, $province){
+
+        $con = $this->opencon();
+        try{
+        $con->beginTransaction();
+        $stmt = $con->prepare("INSERT INTO address (ba_street, ba_barangay, ba_city, ba_province) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$street, $barangay, $city, $province]);
+
+        $addressId = $con->lastInsertId();
+
+        $stmt = $con->prepare("INSERT INTO users_address(user_id, address_id) VALUES (?, ?)");
+        $stmt->execute([$userID, $addressId]);
+
+        $con->commit();
+        return true;
+    
+    }catch (PDOException $e){
+        $con->rollback();
+        return false;
+    }
+
+    }
+}
